f400ae8298194e84a222725bc73fc5fb600da951
Metruption <theaaront222@yahoo.com>
2016-08-02 10:05:50 -0400

    added more unwritten code
    parse_name and parse_direction need to be written. executor has been
    mostly written. basically all the manpypampy architecture has been
    finished and I just need to actually write the code. need to write to
    files and manupulate strings

--- src/tpa_tracker/tpa_tracker.lua
+++ src/tpa_tracker/tpa_tracker.lua
@@ -2,16 +2,14 @@ require "espeak"
 
 
 --@params:
---	speaker is the espeak object that'll do the talker //might not even need those
 --	player is the name of the person whose tpa speaker is about to talk abour
 --	direction is which way the tpa is going. up = true;down = false
 --@preconditions:
---	speaker is a valid espeak objcet //might not even need those
 --	player contains a string
 --	direction contains a boolean
 --@postconditions:
 --	speaks aloud a soundtrigger following the human readable rules in readme.txt
-function tpa_declare (speaker,player,direction)
+function tpa_declare(player,direction)
 	espeak.Synth(player,0,espeak.POS_CHARACTER)
 	if direction then
 		espeak.SetParameter(espeak.PITCH,60,false)
@@ -21,4 +19,51 @@ function tpa_declare (speaker,player,direction)
 	end
 	espeak.SetParameter(espeak.PITCH,50,false)
 
---todo: finish this
+--@params:
+--	line is the line that triggered the trigger
+--	direction is which way the tpa is going. up = true;down = false
+--@preconditions:
+-- line is a string
+-- direction is a boolean
+--@postconditions:
+-- returns which way the tpa is going. up = true;down = false
+--  if trigger misfires on a roomchat returns "rincewind"
+function parse_name(line,direction)
+	return --todo code this
+
+--@params:
+--	line is the line that triggered the trigge
+--@preconditions:
+-- line is a string
+--@postconditions:
+-- returns the name of the person who is having their tpa status modified
+--  if trigger misfires on a roomchat returns "rincewind"
+function parse_direction(line)
+	return --todo code this
+
+--prepare for EXECUTION (you will be executed by npcs if your fi.de.* sucks)
+--better put up another TPA
+--@params:
+-- name - the name of the trigger or alias (the contents of the "label" field)
+-- line - the entire line that matched the trigger or alias
+-- wildcards - an array of the matching wildcards (if any)
+--@preconditions:
+-- name is a string
+-- line is a string
+-- there are no wildcards? (i can probably parse the name that way)
+--@postconditions:
+-- does all the stuff so that readme.txt would be happy if it had emotion
+function executor(name, line)
+	direction = parse_direction(line)
+	if direction == "rincewind"
+		return
+		--todo: make it log that it thinks there was a misfire on the direction
+		--			also include the line in the misfire log
+	name = parse_name(line)
+	if name == "rincewind"
+		return
+		--todo: make it log that it thinks there was a misfire on the name
+		--			also include the line in the misfire log
+
+
+--todo: code those things
