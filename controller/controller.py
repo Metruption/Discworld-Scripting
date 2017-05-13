82f8e0940d4daaa2b0c1930a6343a732a464f5d2
Aaron Thomas <ftk2ne@gmail.com>
2017-05-12 20:11:26 -0400

    Added all remaining bones to the skeleton

--- controller/controller.py
+++ controller/controller.py
@@ -3,23 +3,29 @@ from inputs import get_gamepad
 import json
 
 class RenameThisClass:
-	def __init__():
-		self.buttons_pressed = []
-		with open('dwcontroller.json') as data_file:    
-	    	badname_thing = json.load(data_file)
+    def __init__():
+        self.buttons_pressed = []
+        try:
+            with open('dwcontroller.json') as data_file:    
+                badname_thing = json.load(data_file)
+        except:
+            self.setup()
 
-	def update_pressed_buttons(self, event):
-		pass #@todo(code this)
+    def setup():
+        pass #@todo(code this)
 
-	def send_to_mud(sendthis):
-		pass #@todo(code this)
+    def update_pressed_buttons(self, event):
+        pass #@todo(code this)
 
-	def run(self):
-		while 1:
-		    events = get_gamepad()
-		    for event in events:
-		    	update_pressed_buttons(event)
-		    	try:
-		    		sendthis = badname_thing[''.join(self.buttons_pressed)]
-		    		send_to_mud(sendthis)
-	         
+    def send_to_mud(sendthis):
+        pass #@todo(code this)
+
+    def run(self):
+        while 1:
+            events = get_gamepad()
+            for event in events:
+                self.update_pressed_buttons(event)
+                try:
+                    sendthis = badname_thing[''.join(self.buttons_pressed)]
+                    self.send_to_mud(sendthis)
+             
